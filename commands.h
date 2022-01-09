ac65d547e02d9a70cf4deaac7c123b638fd9c3fa
Sergei <80399042+SERJCOM@users.noreply.github.com>
2022-01-09 16:12:41 +0200

    Update commands.h

--- commands.h
+++ commands.h
@@ -5,19 +5,28 @@
 #include <curl/curl.h> 
 #include <json/json.h>
 #include <string>
+#include <fstream>
 
 
 class Bot {
 public:
-	Bot(std::string key_access);
+	Bot(std::string key_access, std::string id_my_group);
 	~Bot();
 	void update();
 	void send(std::string answer, std::string user_id);
+	void write(std::string message);
+	void conf_init();
+	std::string find_message(std::string message);
+	std::string random_message(int count);
+
 	std::string message;
 	std::string chat_id;
 	std::string user_id;
 	std::string type;
-
+	int group_id;
+	std::string peer_id;
+	int count_lines;
+	
 private:
 	static size_t write_data(char* ptr, size_t size, size_t nmemb, std::string* data);
 	std::string key_access;
@@ -31,4 +40,7 @@ private:
 	std::string ts;
 	std::string content;
 	std::string content_js;
+	std::ofstream outf;
+	std::ifstream inf;
+	
 };
